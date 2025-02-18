c01f618dd210f0fe1dda077e47ffafef4734e7d8
yuya.shiraishi <yuya0505soccer@gmail.com>
2025-02-19 08:38:24 +0900

    ✨一旦コミット

--- src/config/app.php
+++ src/config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Tokyo'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,9 +78,9 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'ja'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'ja'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
+Route::get("tests/test", [TestController::class, "index"]);
 
 Route::get('/', function () {
     return view('welcome');
