61f7d7c384ec74e31f8ab8d9a8748c019ce99cd5
yuya.shiraishi <yuya0505soccer@gmail.com>
2025-02-19 09:53:48 +0900

    ✨コミット

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 
 Route::get("tests/test", [TestController::class, "index"]);
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix("contacts")->middleware(["auth"])->controller(ContactFormController::class)->name("contacts.")->group(function () {
+  Route::get("/", "index")->name("index");
 });
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
